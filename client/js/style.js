d27862b6019daec791d71516eb3abb8bd9b8def5
conniechu929 <conniechu929@gmail.com>
2017-03-23 15:20:10 -0700

    changed commas in js file

--- client/js/style.js
+++ client/js/style.js
@@ -8,17 +8,17 @@ $(document).ready(function() {
             in: { effect: 'fadeInUpBig',
                   shuffle: true,
                   delay: 60
-                },
+                }
         });
 
         $("#enterbtn").textillate({
           loop: true,
           autoStart: true,
           in: { effect: 'flash',
-                delayScale: 2.5,
+                delayScale: 2.5
               },
           out: { effect: 'flash',
-                delayScale: 2.5,
+                delayScale: 2.5
               }
         });
 
@@ -28,11 +28,11 @@ $(document).ready(function() {
           minDisplayTime: 4000,
           in: { effect: "rollIn",
                 shuffle: true,
-                delay: 40,
+                delay: 40
               },
           out: { effect: "hinge",
                  shuffle: true,
-                 delay: 60,
+                 delay: 60
           }
         });
       })
